3c4b70e90995c010e35c756daac8dad372afb791
Zainab Adeola <zadeola@zadeola-ltmcl9h.internal.salesforce.com>
2026-06-16 13:13:49 -0700

    fixing color pallete

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -61,7 +61,7 @@ const MovieCard = ({ movie, onClick, isFavorite, isWatched, onToggleFavorite, on
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
           </svg>
         </button>
 
